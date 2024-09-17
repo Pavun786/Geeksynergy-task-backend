01c6c5c6d95950fb08ab37095f4171fb10c018fb
Pavun786 <pulsarpavuns786@gmail.com>
2024-09-17 17:26:42 +0530

    config : cors

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ dotenv.config()
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
-app.use(cors("*"))
+app.use(cors({
+    origin : "https://geeksynergy-task-frontend.vercel.app"
+}))
 
 dbConnection()
 
